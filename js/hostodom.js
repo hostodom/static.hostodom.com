c7627829bb64c4aaa3d46558bc19fdd2c95d951d
hostodom <66781549+hostodom@users.noreply.github.com>
2020-06-11 19:03:51 +0530

    Update hostodom.js

--- js/hostodom.js
+++ js/hostodom.js
@@ -7,9 +7,5 @@ function openLiveChat() {
 
 // jQuery Functions
 $(document).ready(function() {
-	$('.top-header').prepend('<div class="top-bar-support"><span class="top-support-email"><a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;"><i class="far fa-envelope"></i><span>&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;</span></a></span><span class="top-live-chat"><a href="#live-chat"><i class="far fa-comments"></i><span>Live Chat</span></a></span></div>');
-});
-
-$( "#live-chat" ).click(function() {
-	openLiveChat();
+	$('.top-header').prepend('<div class="top-bar-support"><span class="top-support-email"><a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;"><i class="far fa-envelope"></i><span>&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;</span></a></span><span class="top-live-chat"><a href="#live-chat" onclick="openLiveChat() return false;"><i class="far fa-comments"></i><span>Live Chat</span></a></span></div>');
 });
