2779caa2ec680123162a2d020b9802fc847f3a26
hostodom <66781549+hostodom@users.noreply.github.com>
2020-06-11 18:42:59 +0530

    Update hostodom.js

--- js/hostodom.js
+++ js/hostodom.js
@@ -1,5 +1,5 @@
 // HOSTODOM FUNCTIONS
 
 $(document).ready(function() {
-	$('.top-header').prepend('<div class="top-bar-support"><span class="top-support-email"><i class="far fa-envelope"></i><a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;">&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;</a></span><span class="top-live-chat"><i class="far fa-comments"></i><a href="#live-chat">Live Chat</a></span></div>');
+	$('.top-header').prepend('<div class="top-bar-support"><span class="top-support-email"><a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;"><i class="far fa-envelope"></i>&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;</a></span><span class="top-live-chat"><a href="#live-chat"><i class="far fa-comments"></i>Live Chat</a></span></div>');
 });
