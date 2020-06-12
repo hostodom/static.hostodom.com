c486f457f9edbb93591287b9eac063ce212f334b
hostodom <66781549+hostodom@users.noreply.github.com>
2020-06-12 08:34:49 +0530

    Update hostodom.js

--- js/hostodom.js
+++ js/hostodom.js
@@ -1,11 +1,22 @@
 // HOSTODOM FUNCTIONS
 
+// jQuery Functions
+$(document).ready(function() {
+	$('.top-header').prepend('<div class="top-bar-support"><span class="top-support-email"><a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;"><i class="far fa-envelope"></i><span>&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;</span></a></span><span class="top-live-chat"><a href="#!" onclick="openLiveChat();"><i class="far fa-comments"></i><span>Live Chat</span></a></span></div>');
+});
+
 // Live Chat
 function openLiveChat() {
 	window.open("https://tawk.to/chat/5ee22af74a7c6258179a6569/default", "_blank", "toolbar=no,scrollbars=yes,resizable=yes,top=500,left=500,width=500,height=580");
 }
 
-// jQuery Functions
-$(document).ready(function() {
-	$('.top-header').prepend('<div class="top-bar-support"><span class="top-support-email"><a href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;"><i class="far fa-envelope"></i><span>&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#104;&#111;&#115;&#116;&#111;&#100;&#111;&#109;&#046;&#099;&#111;&#109;</span></a></span><span class="top-live-chat"><a href="#!" onclick="openLiveChat();"><i class="far fa-comments"></i><span>Live Chat</span></a></span></div>');
-});
+// Tawk.to
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+	var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+	s1.async=true;
+	s1.src='https://embed.tawk.to/5ee22af74a7c6258179a6569/default';
+	s1.charset='UTF-8';
+	s1.setAttribute('crossorigin','*');
+	s0.parentNode.insertBefore(s1,s0);
+})();
