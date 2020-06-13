e48dc003ca54e031f06d2a3b53576f9c33dfdeb1
Hostodom <66781549+hostodom@users.noreply.github.com>
2020-06-13 16:49:36 +0530

    Update resellers.js

--- js/resellers.js
+++ js/resellers.js
@@ -1,7 +1,11 @@
 //*** Hostodom Resellers
 
+$(document).ready(function() {
+});
+
 // jQuery Functions
 $(document).ready(function() {
+    $('#introLoading').introLoader();
 	$('#menu0').css('font-size', '18px');
 	$('.home-banner img').width(760);
 });
