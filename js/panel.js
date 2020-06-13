1fd384face5c7dccfbd54c06d83ba86f03b86168
Hostodom <66781549+hostodom@users.noreply.github.com>
2020-06-13 18:02:02 +0530

    Add files via upload

---
+++ js/panel.js
@@ -0,0 +1,6 @@
+//*** HOSTODOM CONTROL PANEL
+
+// jQuery Functions
+$(document).ready(function() {
+	$('head').append('<link rel="stylesheet" href="https://static.hostodom.com/css/panel.css">');
+});
