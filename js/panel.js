078e33f4d954a6a6b541266c62dacaa7027efaa1
Hostodom <66781549+hostodom@users.noreply.github.com>
2020-06-13 18:41:41 +0530

    Update panel.js

--- js/panel.js
+++ js/panel.js
@@ -2,8 +2,6 @@
 
 // jQuery Functions
 $(document).ready(function() {
-	$('head').append('<link rel="icon shortcut" href="https://static.hostodom.com/img/favicon.png">');
-	$('head').append('<link rel="stylesheet" href="https://static.hostodom.com/css/panel.css">');
 	$.fakeLoader({
 		bgColor: '#fff',
 		spinner: 'spinner2'
