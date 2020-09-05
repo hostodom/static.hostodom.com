efcd4b819f2857e2b3169ad4454ad366294f2898
Hostodom <66781549+hostodom@users.noreply.github.com>
2020-09-05 20:05:36 +0530

    Update panel.js

--- js/panel.js
+++ js/panel.js
@@ -6,8 +6,21 @@ $(document).ready(function() {
 
 });
 
+// Add Favicon
+var linkFavicon = document.createElement("link");
+linkFavicon.rel = "shortcut icon";
+linkFavicon.href = "https://static.hostodom.com/img/favicon.png";
+document.head.appendChild(linkFavicon);
+
 // Add Panel CSS
 var linkPanelCss = document.createElement("link");
 linkPanelCss.rel = "stylesheet";
 linkPanelCss.href = "https://static.hostodom.com/css/panel.css";
 document.head.appendChild(linkPanelCss);
+
+// Add Footer Text
+document.addEventListener("DOMContentLoaded", function(){
+	var current_year = new Date().getFullYear();
+	var footer_copyright = document.getElementById("footer-wrapper");
+	footer_copyright.prepend('<div class="footer-copyright">&copy; ' + current_year + ' Hostodom. All rights reserved.');
+});
