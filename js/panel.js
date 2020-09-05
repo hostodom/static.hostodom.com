2d8af68f58dbaee8a0c11938a2d39eef4b35224f
Hostodom <66781549+hostodom@users.noreply.github.com>
2020-09-05 19:36:27 +0530

    Update panel.js

--- js/panel.js
+++ js/panel.js
@@ -1,7 +1,13 @@
-//** HOSTODOM FUNCTIONS
+//*** HOSTODOM CONTROL PANEL
 
 // jQuery Functions
 $(document).ready(function() {
 	$('#preloader').fade();
 
 });
+
+// Add Panel CSS
+var linkPanelCss = document.createElement("link");
+linkPanelCss.rel = "stylesheet";
+linkPanelCss.href = "https://static.hostodom.com/css/panel.css";
+document.head.appendChild(linkPanelCss);
